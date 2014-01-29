a42f9fc40a43add4db12965e45365947497b8b56
fehu <lord.fehu@gmail.com>
2014-01-28 22:12:42 -0600

    renamed project; it will be a collection of DSLs

--- project/Build.scala
+++ project/Build.scala
@@ -10,34 +10,47 @@ object  Build extends sbt.Build {
   import Dependencies._
 
   val buildSettings = Defaults.defaultSettings ++ Seq (
-    organization  := "feh",
-    version       := Version,
+    organization  := "feh.dsl",
     scalaVersion  := ScalaVersion,
 //    scalacOptions ++= Seq("-explaintypes"),
 //    scalacOptions ++= Seq("-deprecation"),
     scalacOptions in (Compile, doc) ++= Seq("-diagrams", "-diagrams-debug")
-//     mainClass in Compile := Some("")
   )
 
   // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // //
 
   object Dependencies{
     lazy val scalaSwing = "org.scala-lang" % "scala-swing" % ScalaVersion
-//    lazy val reflectApi = "org.scala-lang" % "scala-reflect" % ScalaVersion
+    lazy val reflectApi = "org.scala-lang" % "scala-reflect" % ScalaVersion
 
     object feh{
-      lazy val util = "feh" %% "util" % "1.0"
+      lazy val util = "feh" %% "util" % "1.0.1"
     }
   }
 
   // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // //
 
+  lazy val root = Project(
+    id = "dsl-root",
+    base = file(".")
+  ).settings(ideaExcludeFolders := ".idea" :: ".idea_modules" :: Nil)
+   .aggregate(swing, graphviz)
+
   lazy val swing = Project(
-    id = "swing-dsl",
-    base = file("."),
+    id = "swing",
+    base = file("swing"),
     settings = buildSettings ++ Seq(
+      version := "1.0",
       libraryDependencies ++= Seq(scalaSwing, feh.util)
     )
-  ).settings(ideaExcludeFolders := ".idea" :: ".idea_modules" :: Nil)
+  )
 
+  lazy val graphviz = Project(
+    id = "graphviz",
+    base = file("graphviz"),
+    settings = buildSettings ++ Seq(
+      version := "0.1",
+      libraryDependencies += feh.util
+    )
+  )
 }
