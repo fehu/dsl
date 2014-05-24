3a992c39ba18b1c382ac94f651ef258f651a1381
fehu <lord.fehu@gmail.com>
2014-05-24 15:17:08 -0500

    dependencies fix for scala version 2.11.1: no separate swing lib present in maven, "scala-library-all" is used instead.

--- project/Build.scala
+++ project/Build.scala
@@ -6,7 +6,7 @@ object Build extends sbt.Build {
 
   val commonBuildSettings = Defaults.defaultSettings ++ Seq (
     organization  := "feh.dsl",
-    crossScalaVersions := Seq("2.10.3", "2.11.1"),
+    crossScalaVersions := Seq("2.11.1", "2.10.3"),
       //    scalacOptions ++= Seq("-explaintypes"),
 //    scalacOptions ++= Seq("-deprecation"),
     scalacOptions in (Compile, doc) ++= Seq("-diagrams")
@@ -15,7 +15,11 @@ object Build extends sbt.Build {
   // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // //
 
   object Dependencies{
-    def scalaSwing(version: String) = "org.scala-lang" % "scala-swing" % version
+    def scalaSwing(version: String) = version match { // there is no "scala-swing" for 2.11.1 now, it's in lib-all
+      case v if v startsWith "2.10" => "org.scala-lang" % "scala-swing" % v
+      case v if v startsWith "2.11" => scalaLibAll(v)
+    }
+    def scalaLibAll(version: String) = "org.scala-lang" % "scala-library-all" % version // 2.11.x
     def reflectApi(version: String) = "org.scala-lang" % "scala-reflect" % version
 
     object feh{
@@ -27,7 +31,8 @@ object Build extends sbt.Build {
 
   lazy val root = Project(
     id = "dsl-root",
-    base = file(".")
+    base = file("."),
+    settings = commonBuildSettings
   ).settings(ideaExcludeFolders := ".idea" :: ".idea_modules" :: Nil)
    .aggregate(swing, graphviz)
 
